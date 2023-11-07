7e7f43e642b644f53e965f96e2d3348768160f54
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-11-07 21:19:34 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div>
-      <Article />
-      <Comment />
+      <NavBar />
+      <Home />
+      <About/>
     </div>
   );
 }
